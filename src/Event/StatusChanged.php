bfc1ab79531826e1d1ba6dd2cbca776b39c3e038
hdj <hdj@addcn.com>
2021-04-02 15:40:38 +0800

    add event

---
+++ src/Event/StatusChanged.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of websocket-cluster-addon.
+ *
+ * @link     https://github.com/friendofhyperf/websocket-cluster-addon
+ * @document https://github.com/friendofhyperf/websocket-cluster-addon/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ * @license  https://github.com/friendofhyperf/websocket-cluster-addon/blob/main/LICENSE
+ */
+namespace FriendsOfHyperf\WebsocketClusterAddon\Event;
+
+class StatusChanged
+{
+    /**
+     * @var int
+     */
+    public $uid;
+
+    /**
+     * @var int
+     */
+    public $status;
+
+    public function __construct(int $uid, int $status)
+    {
+        $this->uid = $uid;
+        $this->status = $status;
+    }
+}
--- src/Provider/RedisOnlineProvider.php
+++ src/Provider/RedisOnlineProvider.php
@@ -11,8 +11,10 @@ declare(strict_types=1);
  */
 namespace FriendsOfHyperf\WebsocketClusterAddon\Provider;
 
+use FriendsOfHyperf\WebsocketClusterAddon\Event\StatusChanged;
 use Hyperf\Redis\RedisFactory;
 use Psr\Container\ContainerInterface;
+use Psr\EventDispatcher\EventDispatcherInterface;
 
 class RedisOnlineProvider implements OnlineProviderInterface
 {
@@ -31,19 +33,45 @@ class RedisOnlineProvider implements OnlineProviderInterface
      */
     protected $prefix = 'wssa:online';
 
+    /**
+     * @var EventDispatcherInterface
+     */
+    protected $eventDispatcher;
+
+    /**
+     * @var ClientProviderInterface
+     */
+    private $clientProvider;
+
     public function __construct(ContainerInterface $container)
     {
         $this->redis = $container->get(RedisFactory::class)->get($this->redisPool);
+        $this->eventDispatcher = $container->get(EventDispatcherInterface::class);
+        $this->clientProvider = $container->get(ClientProviderInterface::class);
     }
 
     public function add(int $uid): void
     {
+        if ($this->get($uid)) {
+            return;
+        }
+
         $this->redis->sAdd($this->getKey(), $uid);
+        $this->eventDispatcher->dispatch(new StatusChanged($uid, 1));
     }
 
     public function del(int $uid): void
     {
+        if (! $this->get($uid)) {
+            return;
+        }
+
+        if ($this->clientProvider->size($uid) > 0) {
+            return;
+        }
+
         $this->redis->sRem($this->getKey(), $uid);
+        $this->eventDispatcher->dispatch(new StatusChanged($uid, 0));
     }
 
     public function get(int $uid): bool
