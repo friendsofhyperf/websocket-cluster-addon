c8de7d82c59948f8fd35bd16034a937ffc1b310a
hdj <hdj@addcn.com>
2021-04-09 11:41:00 +0800

    add monitoring

--- src/Controller/InfoController.php
+++ src/Controller/InfoController.php
@@ -12,14 +12,10 @@ declare(strict_types=1);
 namespace FriendsOfHyperf\WebsocketClusterAddon\Controller;
 
 use FriendsOfHyperf\WebsocketClusterAddon\Addon;
-use FriendsOfHyperf\WebsocketClusterAddon\Connection\ConnectionInterface;
-use FriendsOfHyperf\WebsocketClusterAddon\Provider\ClientProviderInterface;
 use FriendsOfHyperf\WebsocketClusterAddon\Provider\OnlineProviderInterface;
-use Hyperf\Contract\ConfigInterface;
 use Hyperf\HttpServer\Annotation\Controller;
 use Hyperf\HttpServer\Annotation\GetMapping;
 use Hyperf\HttpServer\Contract\RequestInterface;
-use Hyperf\Redis\RedisFactory;
 use Psr\Container\ContainerInterface;
 
 /**
@@ -31,16 +27,6 @@ class InfoController
      * @var ContainerInterface
      */
     protected $container;
-
-    /**
-     * @var \Redis
-     */
-    protected $redis;
-
-    /**
-     * @var ConfigInterface
-     */
-    protected $config;
 
     /**
      * @var RequestInterface
@@ -57,26 +43,12 @@ class InfoController
      */
     protected $onlineProvider;
 
-    /**
-     * @var ClientProviderInterface
-     */
-    protected $clientProvider;
-
-    /**
-     * @var ConnectionInterface
-     */
-    protected $connectionProvider;
-
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
-        $this->config = $container->get(ConfigInterface::class);
-        $this->redis = $container->get(RedisFactory::class)->get($this->config->get('websocket_cluster.server.pool', 'default'));
         $this->request = $container->get(RequestInterface::class);
         $this->addon = $container->get(Addon::class);
         $this->onlineProvider = $container->get(OnlineProviderInterface::class);
-        $this->clientProvider = $container->get(ClientProviderInterface::class);
-        $this->connectionProvider = $container->get(ConnectionInterface::class);
     }
 
     /**
@@ -89,45 +61,9 @@ class InfoController
 
             return [
                 'online' => $this->onlineProvider->get($uid),
-                'clients' => $this->clientProvider->size($uid),
             ];
         }
 
-        $redis = $this->redis;
-        $servers = $this->addon->getServers();
-        $callbacks = [];
-
-        foreach ($servers as $pod) {
-            $callbacks[] = function () use ($redis, $pod) {
-                $connections = 0;
-                $pattern = sprintf('%s:%s:%s', $this->config->get('websocket_cluster.client.prefix'), $pod, '*');
-
-                $users = collect($redis->keys($pattern))
-                    ->each(function ($key) use ($redis, &$connections) {
-                        $connections += $redis->sCard($key);
-                    })
-                    ->count();
-
-                return [
-                    'pod' => $pod,
-                    'users' => $users,
-                    'connections' => $connections,
-                ];
-            };
-        }
-
-        return parallel($callbacks);
-    }
-
-    /**
-     * @GetMapping(path="node")
-     */
-    public function node()
-    {
-        return [
-            'name' => gethostname(),
-            'users' => $this->connectionProvider->users(),
-            'connections' => $this->connectionProvider->size(0),
-        ];
+        return $this->addon->getMonitors();
     }
 }
